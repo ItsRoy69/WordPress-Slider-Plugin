c005f2ab7340acd875c27100e27467a9bf53a91f
itsroy69 <jyotirmoyroy649@gmail.com>
2024-04-09 14:22:33 +0530

    dding constants.js for mentioning constant components, edit.js file is where we establish all the state and logic for everything the user will see and interact with in the editor

---
+++ src/constants.js
@@ -0,0 +1,52 @@
+export const ALLOWED_BLOCKS = [ 'core/cover' ];
+export const DEFAULT_BLOCK = 'core/cover';
+export const DEFAULT_BLOCK_ATTRIBUTES = {
+	align: 'center',
+	className: 'swiper-slide',
+	contentPosition: 'bottom left',
+	customOverlayColor: '#000000',
+	dimRatio: 20,
+	layout: {
+		type: 'constrained',
+	},
+	style: {
+		color: {
+			text: '#ffffff',
+		},
+		elements: {
+			heading: {
+				color: {
+					text: '#ffffff',
+				},
+			},
+			link: {
+				color: {
+					text: '#ffffff',
+				},
+			},
+		},
+		spacing: {
+			padding: {
+				top: 'var:preset|spacing|large',
+				bottom: 'var:preset|spacing|large',
+				left: 'var:preset|spacing|large',
+				right: 'var:preset|spacing|large',
+			},
+		},
+	},
+};
+
+export const DEFAULT_INNERBLOCK = 'core/paragraph';
+
+export const DEFAULT_INNERBLOCK_ATTRIBUTES = {
+	fontSize: 'large',
+	style: {
+		color: {
+			text: '#ffffff',
+		},
+	},
+};
+
+export const PLACEHOLDER_IMG_1 = 'https://images.unsplash.com/photo-1712146545481-541069ca6785';
+export const PLACEHOLDER_IMG_2 = 'https://images.unsplash.com/photo-1712105052319-c900e970467a';
+export const PLACEHOLDER_IMG_3 = 'https://images.unsplash.com/photo-1712371962371-01564121d424';
